1f80de571dce528a4b20d6893be243cd15d213fc
Victoria <viky_lechka@mail.ru>
2022-10-03 20:38:28 +0300

    Solution Ex2

--- Ex2/Program.cs
+++ Ex2/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("He22llo, World!");
+﻿/*Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3 */
+int a,b;
+Console.Write("Введите первое число, ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число, ");
+b = Convert.ToInt32(Console.ReadLine());
+int max = a;
+    if(max > b)
+    {
+        Console.WriteLine("max = {0}", max);
+
+        Console.WriteLine("min = {0}", b);
+    }
+
+    else
+    {
+        Console.WriteLine("max = {0}", b);
+
+        Console.WriteLine("min = {0}", a); 
+    }
