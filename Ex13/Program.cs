a7f41d02a551ecb7cf6dd487bb2d64af82764028
Victoria <viky_lechka@mail.ru>
2022-10-06 14:05:10 +0300

    Exercise13

---
+++ Ex13/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6 */
+
+Console.WriteLine("Введите положительное число, ");
+int n = Convert.ToInt32(Console.ReadLine());
+int amount = n.ToString().Length;
+
+if(amount < 3 || n < 0)
+{
+    Console.WriteLine("Третьей цифры нет, или вы ввели отрицательное число");
+}
+else
+{
+    Console.WriteLine(n.ToString()[2]);
+}
