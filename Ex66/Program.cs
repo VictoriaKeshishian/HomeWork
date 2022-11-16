e11a19e7f4ae2c90606ed9cfe2d902a523b49cb7
Victoria <viky_lechka@mail.ru>
2022-11-16 16:22:01 +0300

    sum via recursion

---
+++ Ex66/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов 
+в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+Console.WriteLine("Введите числа");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+
+int CalculateSumm(int m, int n)
+		{
+			int start = m;
+			int end = n;
+			if(m > n)
+			{
+				start = n;
+				end = m;
+			}
+            return ((start + end) * end / 2); // формулf суммы членов арифметической прогрессии
+		}
+		
+		Console.WriteLine(CalculateSumm(m, n));
